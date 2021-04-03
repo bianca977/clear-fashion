b5c2d3d4fb19c8607a90bb8458c12995d5eceb3d
Bianca <bianca@MacBook-Air-de-B.local>
2021-04-03 13:07:45 +0200

    api

--- server/api.js
+++ server/api.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
-const db= require('./db/index');
+const db= require('./db');
 
 const dbName='clearfashion';
 
@@ -27,13 +27,37 @@ app.get('/', (request, response) => {
 });
 
 //endpoint pour ajouter des filtres 
+app.get('/products', async (req, res) => {
+  //response.send({'a': true});
+  let page = parseInt(req.query.page);
+  let size = parseInt(req.query.size);
+  let start = (size*(page-1));
+  console.log("start= "+start);
+  console.log("end=" +start + size);
+  let prod = []
+  let counter = 0;
+  const result = await db.find({"price":{$ne:Number("Nan")}})
+
+  for(i=start;i<start+size;i++){
+      if(result[i] != null){
+        console.log(i+' '+result[i].price)
+        prod.push(result[i])
+        counter++;
+
+      }
+
+    }
+  console.log(counter);
+  res.send({"success":true,"data":{"result":prod,"meta":{"currentPage":page,"pageCount":Math.round(result.length/size),"pageSize":size,"count":result.length}}});
+});
+
 app.get('/products/search', async (req, res) => {
   let limit = 12
   if(req.query.limit){
     limit = req.query.price;
   }
   let prod = []
-  const result = await querydata({'brand':req.query.brand,'price':{"$lte": parseFloat(req.query.price)}})
+  const result = await db.find({'brand':req.query.brand,'price':{"$lte": parseFloat(req.query.price)}})
   for(i=0;i<limit;i++){
     prod.push(result[i])
   }
